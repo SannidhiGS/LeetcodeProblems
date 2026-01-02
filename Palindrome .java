7c4eac6b86dbcab56f3ab35202844869f748ab4b
Sannidhi G S <128506865+SannidhiGS@users.noreply.github.com>
2026-01-02 23:03:51 +0530

    Implement palindrome check for integers

---
+++ Palindrome .java
@@ -0,0 +1,23 @@
+//A number is a palindrome if it reads the same forward and backward.
+class Palindrome {
+    public boolean isPalindrome(int x) {
+        //for negative number
+        if(x<0){
+            return false;
+        }
+        //assign value of x to n
+        int n=x;
+        int revNum=0;
+        while(n>0){
+            int d=n%10;
+            revNum=revNum*10+d;
+            n=n/10;
+        }
+        if(revNum==x){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
