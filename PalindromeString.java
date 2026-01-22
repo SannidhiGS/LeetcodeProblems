6a88ffdf587fbfc542a9e8f110c9cb68bf6f3b8b
Sannidhi G S <128506865+SannidhiGS@users.noreply.github.com>
2026-01-22 13:10:40 +0530

    Add PalindromeString class with isPalindrome method
    Replace PalindromeString this with Solution

---
+++ PalindromeString.java
@@ -0,0 +1,15 @@
+class PalindromeString {
+    public boolean isPalindrome(String s) {
+        s=s.toLowerCase().replaceAll("[^a-z0-9]","");
+        String rev="";
+        for(int i=s.length()-1;i>=0;i--){
+            rev=rev+s.charAt(i);
+        }
+        if(s.equals(rev)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
